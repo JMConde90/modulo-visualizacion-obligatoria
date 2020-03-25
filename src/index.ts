5c22f9ea3d443a6d68c8c7c2e1f83b1b53f3d23b
Juan Mc <juanmc@MacBook-Pro-de-Juan.local>
2020-03-25 18:43:53 +0100

    ultima version 25/03/2020

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,60 @@
 import * as d3 from "d3";
 import * as topojson from "topojson-client";
-const spainjson = require("./spain.json");
 const d3Composite = require("d3-composite-projections");
-import { latLongCommunities } from "./communities";
-import { statsBase } from "./stats";
-import { stats22Marzo } from "./stats";
+const europejson = require("./europe.json");
+const spainjson = require("./spain.json")
 
-//Se calcula el numero maxima de afectados de todas
-//las comunidades
-const maxAffected = statsBase.reduce(
-  (max, item) => (item.value > max ? item.value : max),
-  0
-);
-// creo los circulos para mostrar
-const affectedRadiusScale = d3
-  .scaleQuantile()
-  .domain([0, maxAffected])
-  .range([10,20,30,40,40]); //rango de valores a asiganr
-                            //para el domino, hace tantas
-                            //particiones como rangos se le
-                            //indique
-
-
-  const calculateRadiusBasedOnAffectedCases = (comunidad: string, currentStats: any[]) => {
-    const entry = currentStats.find(item => item.name === comunidad);
-    let size = affectedRadiusScale(entry.value);
+ import { stats22Marzo } from  "./stats";
+// Esacala de colores para el numero de afectados por comunidad 
+var color = d3
+  .scaleThreshold<number, string>()
+  .domain([200,500,700,1000,1500,1700,2000,2100,5000,10000])
+  .range([
+    "#ff0505",
+    "#ff1919",
+    "#ff2c2c",
+    "#ff4040",
+    "#ff5353",
+    "#ff6767",
+    "#ff7b7b",
+    "#ff8e8e",
+    "#ffa2a2",
+    "#ffb6b6",
+    "#ffc9c9",
+    "#ffdddd",
+    "#fff0f0"
     
-    if(entry) {
-        size = affectedRadiusScale(entry.value);
+  ]);
+  console.log(color)
+/*
+var color = d3
+  .scaleThreshold<number, string>()
+  .domain([0, 1, 100, 500, 700, 20000])
+  .range([
+    "#FFFFF",
+    "#FFE8E5",
+    "#F88F70",
+    "#CD6A4E",
+    "#A4472D",
+    "#7B240E",
+    "#540000"
+  ]);
+*/
+//funcion para asignar colores a las comunidades
+const assignRegionBackgroundColor = (RegionName: string) => {
+  const item = stats22Marzo.find(
+    item => item.name === RegionName
+  );
+  /*
+  if (item) {
+    console.log(item.value);
+  }
+  */
+   //lo pongo el ternario por si hay fallo me devuelve color 0
+  return item ? color(item.value) : color(0);
+};
 
-    }
-    
-    
-    return size;
-  };
+
 
 const svg = d3
   .select("body")
@@ -41,7 +62,9 @@ const svg = d3
   .attr("width", 1024)
   .attr("height", 800)
   .attr("style", "background-color: #FBFAF0");
+/*
 
+*/
 const aProjection = d3Composite
   .geoConicConformalSpain()
   // Let's make the map bigger to fit in our resolution
@@ -50,46 +73,24 @@ const aProjection = d3Composite
   .translate([500, 400]);
 
 const geoPath = d3.geoPath().projection(aProjection);
-const geojson = topojson.feature(spainjson, spainjson.objects.ESP_adm1);
-//Selecciono todos las comunidades 
+
+
+const geojson = topojson.feature(
+  spainjson,
+  spainjson.objects.ESP_adm1
+);
+
+
+
 svg
   .selectAll("path")
   .data(geojson["features"])
   .enter()
   .append("path")
-  .attr("class", "country")
+  .attr("class", "country") //defino los bordes de las comunidades
+  .style("fill", function(d: any) {
+   // console.log(d.properties.geounit);
+    return assignRegionBackgroundColor(d.properties.NAME_1);
+  })
   // data loaded from json file
   .attr("d", geoPath as any);
-
-//Creo los circulos de afectados por cada comunidad
-svg
-  .selectAll("circle")
-  .data(latLongCommunities)
-  .enter()
-  .append("circle")
-  .attr("class", "affected-marker")
-  .attr("r", d => calculateRadiusBasedOnAffectedCases(d.name,statsBase))
-  .attr("cx", d => aProjection([d.long, d.lat])[0])
-  .attr("cy", d => aProjection([d.long, d.lat])[1])
-  
-  const updateCircles = (data: any[]) => {
-    const circles = svg.selectAll("circle");
-    circles
-      .data(latLongCommunities)
-      .merge(circles as any)
-      .transition()
-      .duration(500)
-      .attr("r", d => calculateRadiusBasedOnAffectedCases(d.name, data));
-  };
-
-  document
-  .getElementById("base")
-  .addEventListener("click", function handleResultsBase() {
-    updateCircles (statsBase );
-  });
-
-  document
-  .getElementById("22marzo")
-  .addEventListener("click", function handleResults22Marzo() {
-    updateCircles (stats22Marzo);
-  });
